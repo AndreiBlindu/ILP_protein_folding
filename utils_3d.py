9ada5f4f7d431ef9178e3efe734c34acd8aa35f4
AndreiBlindu <andreiblindu@live.it>
2023-05-28 14:31:00 +0200

    fixed neighboring_set_3d

--- utils_3d.py
+++ utils_3d.py
@@ -26,3 +26,14 @@ def get_feasible_set(m, n):
             if w == v-1 or w == v+1 or w == v-n or w == v+n:
                 E.append((v,w))
     return E
+
+def neighboring_set_3d(v, l_size):
+    lattice_size = l_size*l_size*l_size
+    l2 = l_size*l_size
+    n_set = []
+
+    for k in range(lattice_size):
+        if (k == v-1 and (k+1)%l_size != 0) or (k == v+1 and k%l_size != 0) or (k == v-l_size and k // l2 == v // l2) or (k == v+l_size and k // l2 == v // l2) or k == v+l2 or k == v-l2 :
+            n_set.append(k)
+    
+    return n_set
